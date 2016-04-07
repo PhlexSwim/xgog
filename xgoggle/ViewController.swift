26b98ee6e7e9e0608b78f248ba747f39fb9d4083
hellyeah <dave@hackmatch.com>
2016-04-07 17:53:25 -0400

    Add ARPieChart framework; implement basic pie chart view below line graph

--- xgoggle/ViewController.swift
+++ xgoggle/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 import SwiftCharts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARPieChartDelegate, ARPieChartDataSource {
 
+    @IBOutlet weak var PieChartView: ARPieChart!
     @IBOutlet weak var LineGraphView: UIView!
+
+    internal var outerRadius: CGFloat = 0.0
+    internal var innerRadius: CGFloat = 0.0
+    internal var selectedPieOffset: CGFloat = 0.0
+    internal var labelFont: UIFont = UIFont.systemFontOfSize(10)
+    internal var showDescriptionText: Bool = false
+    internal var animationDuration: Double = 1.0
+
+    var dataItems: NSMutableArray = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Line Graph Chart
         let chartConfig = ChartConfigXY(
             xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
             yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
@@ -33,6 +45,17 @@ class ViewController: UIViewController {
         )
 
         self.LineGraphView.addSubview(chart.view)
+
+        //Pie Chart
+        PieChartView.delegate = self
+        PieChartView.dataSource = self
+        PieChartView.showDescriptionText = true
+
+        // Random Default Value
+        let defaultItemCount = randomInteger(1, upper: 10)
+        for _ in 1...defaultItemCount {
+            dataItems.addObject(randomItem())
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +63,90 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        PieChartView.reloadData()
+    }
 
+    //Random Functions
+
+    func randomColor() -> UIColor {
+        let randomR: CGFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        let randomG: CGFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        let randomB: CGFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        return UIColor(red: randomR, green: randomG, blue: randomB, alpha: 1)
+    }
+
+    func randomInteger(lower: Int, upper: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(upper - lower + 1))) + lower
+    }
+
+    func randomItem() -> PieChartItem {
+        let value = CGFloat(randomInteger(1, upper: 10))
+        let color = randomColor()
+        let description = "\(value)"
+        return PieChartItem(value: value, color: color, description: description)
+    }
+
+    // #Pie Chart implementation
+    /**
+    *  MARK: ARPieChartDelegate
+    */
+    func pieChart(pieChart: ARPieChart, itemSelectedAtIndex index: Int) {
+        //let itemSelected: PieChartItem = dataItems[index] as! PieChartItem
+        //selectionLabel.text = "Value: \(itemSelected.value)"
+        //selectionLabel.textColor = itemSelected.color
+    }
+
+    func pieChart(pieChart: ARPieChart, itemDeselectedAtIndex index: Int) {
+        //selectionLabel.text = "No Selection"
+        //selectionLabel.textColor = UIColor.blackColor()
+    }
+
+
+    /**
+    *   MARK: ARPieChartDataSource
+    */
+    func numberOfSlicesInPieChart(pieChart: ARPieChart) -> Int {
+        return dataItems.count
+    }
+
+    func pieChart(pieChart: ARPieChart, valueForSliceAtIndex index: Int) -> CGFloat {
+        let item: PieChartItem = dataItems[index] as! PieChartItem
+        return item.value
+    }
+
+    func pieChart(pieChart: ARPieChart, colorForSliceAtIndex index: Int) -> UIColor {
+        let item: PieChartItem = dataItems[index] as! PieChartItem
+        return item.color
+    }
+
+    func pieChart(pieChart: ARPieChart, descriptionForSliceAtIndex index: Int) -> String {
+        let item: PieChartItem = dataItems[index] as! PieChartItem
+        return item.description ?? ""
+    }
+
+
+}
+
+/**
+*  MARK: Pie chart data item
+*/
+public class PieChartItem {
+
+    /// Data value
+    public var value: CGFloat = 0.0
+
+    /// Color displayed on chart
+    public var color: UIColor = UIColor.blackColor()
+
+    /// Description text
+    public var description: String?
+
+    public init(value: CGFloat, color: UIColor, description: String?) {
+        self.value = value
+        self.color = color
+        self.description = description
+    }
 }
 
