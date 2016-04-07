9978d7eee13b18f5cc2e7526a1b0aef8b1a46a5e
hellyeah <dave@hackmatch.com>
2016-04-07 09:38:56 -0400

    add initial graph with SwiftCharts; remove screenshot of overview view

--- xgoggle/ViewController.swift
+++ xgoggle/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import SwiftCharts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let chartConfig = ChartConfigXY(
+            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
+        )
+
+        let chart = LineChart(
+            frame: CGRectMake(0, 70, 300, 500),
+            chartConfig: chartConfig,
+            xTitle: "Time",
+            yTitle: "Heart Rate",
+            lines: [
+                (chartPoints: [(2.0, 10.6), (4.2, 5.1), (7.3, 3.0), (8.1, 5.5), (14.0, 8.0)], color: UIColor.redColor()),
+                (chartPoints: [(2.0, 2.6), (4.2, 4.1), (7.3, 1.0), (8.1, 11.5), (14.0, 3.0)], color: UIColor.blueColor())
+            ]
+        )
+
+        self.view.addSubview(chart.view)
     }
 
     override func didReceiveMemoryWarning() {
